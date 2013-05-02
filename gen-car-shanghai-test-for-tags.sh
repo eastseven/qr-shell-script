58713f6bab0d7166b3fc7ec9d1eb0c47518a1b18
eastseven <eastseven@foxmail.com>
2013-05-02 10:16:59 +0800

    update

--- gen-car-shanghai-test-for-tags.sh
+++ gen-car-shanghai-test-for-tags.sh
@@ -9,7 +9,7 @@ project_name=QuickRide-Car-Client
 cd $project_name
 ant profile-test-shanghai main
 var_date=`date "+%Y%m%d%H%M%S"`
-apk_file_release=qr-car-$version-test.apk
+apk_file_release=qr-car-$var_date-test.apk
 cp target/$apk_file ../$apk_file_release
 scp -r ../$apk_file_release root@www.idingche.com.cn:/var/www/html/apkfile/test
 echo "done..."
