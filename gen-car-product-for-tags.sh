28a113d33ee669b5b85690182218ee4b879d5633
eastseven <eastseven@foxmail.com>
2013-05-02 10:44:10 +0800

    update

--- gen-car-product-for-tags.sh
+++ gen-car-product-for-tags.sh
@@ -10,7 +10,7 @@ sh download-for-tags.sh $version
 cd $project_name
 ant profile-product main
 var_date=`date "+%Y%m%d%H%M%S"`
-apk_file_release=qr-car-$version-product.apk
+apk_file_release=qr-car-$var_date-product.apk
 cp target/$apk_file ../$apk_file_release
 scp -r ../$apk_file_release root@www.idingche.com.cn:/var/www/html/apkfile/product
 echo "done..."
