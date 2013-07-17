b7dea6d7aa08513b25894b4f4235397b1fd36fad
eastseven <eastseven@foxmail.com>
2013-07-17 18:20:59 +0800

    update

--- generate-passenger-apk-for-product.sh
+++ generate-passenger-apk-for-product.sh
@@ -5,6 +5,7 @@ rm -rf *-passenger-*.apk
 
 project_name=QuickRide-Passenger-Client
 cd $project_name
+sh refresh.sh
 ant profile-product main
 var_date=`date "+%Y%m%d%H%M%S"`
 cp target/$apk_file ../qr-passenger-$var_date-product.apk
